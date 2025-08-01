f3e7165dda962d132ba81a053a0fe0c5eb55aaae
Arjun Saxena <arjunsaxena04@gmail.com>
2025-08-01 23:37:02 +0530

    FIXES

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,10 +2,13 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    DATABASE_URL_NEON: str
+    SECRET_KEY: str
     UPLOADS_DIR: str = "uploads"
     GEMINI_API_KEY: str
 
     class Config:
         env_file = ".env"
+        extra = "allow"
 
 settings = Settings() 
